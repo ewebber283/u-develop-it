a0346518504cdde784e49445b03555c21c6e3796
Eric Webber <ewebber283@gmail.com>
2022-05-19 17:29:01 -0500

    created read,create, and delete routes

--- server.js
+++ server.js
@@ -18,15 +18,40 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
+/*db.query(`SELECT * FROM candidates`, (err, rows) => {
   console.log(rows);
+}); */
+
+db.query(`SELECT * FROM candidates WHERE id=1`, (err, rows) => {
+  if(err) {
+    console.log(err)
+  }
+  console.log(rows)
 });
+
+/*db.query(`DELETE FROM candidates WHERE id=?`, 1, (err, rows) => {
+  if(err) {
+    console.log(err)
+  }
+  console.log(rows)
+}); */
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+//create 
+const sql = `INSERT INTO candidates(id, first_name, last_name, industry_connected)
+  values(?,?,?,?)`;
 
+const params = [1, 'Ronald', 'Firbank', 1]
+
+db.query(sql, params, (err, result) => {
+  if(err) {
+    console.log(err)
+  }
+  console.log(result)
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
